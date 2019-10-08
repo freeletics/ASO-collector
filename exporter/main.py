d534673d4249af46408f2832f16fcaa0c12fd145
Marcin Kopec <marcinkopec@GLOVO-C294.local>
2019-10-08 05:18:46 +0200

    [ASO-2]: Init main script file.

---
+++ exporter/main.py
@@ -0,0 +1,14 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def run():
+    pass
+
+
+if __name__ == "__main__":
+    print("Running script")
+    run()
+    print("End of script")
+
---
+++ tests/test_main.py
@@ -0,0 +1,4 @@
+from exporter import main
+
+def test_run_do_not_fail():
+    main.run()
