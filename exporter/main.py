a3620f93e95e4dac605c2518d3decbac52058fd4
Marcin Kopec <marcinkopec@GLOVO-C294.local>
2019-10-17 23:26:20 +0200

    [ASO-2] Add play store script to main.

--- exporter/main.py
+++ exporter/main.py
@@ -1,10 +1,19 @@
 import logging
+from datetime import datetime
+from exporter import config
+from exporter.play_store import script as play_store_script
 
 logger = logging.getLogger(__name__)
 
 
 def run():
-    pass
+    get_play_store_export()
+
+
+def get_play_store_export():
+    logger.info("Reading report from last run call")
+    play_store_script.run(config.DEFAULT_EXPORT_FROM, datetime.now())
+    logger.info("Task success")
 
 
 if __name__ == "__main__":
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,8 @@
+from unittest import mock
 from exporter import main
 
-def test_run_do_not_fail():
+
+@mock.patch("exporter.main.get_play_store_export")
+def test_run_do_not_fail(play_store_mock):
     main.run()
+    assert play_store_mock.called
