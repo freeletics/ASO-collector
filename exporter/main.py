61c96b3386f6dbf74e4b99c7798faf6a1212e681
Marcin Kopec <marcinkopec@GLOVO-C294.local>
2019-10-28 21:59:07 +0100

    [ASO-10]: Update main script.

--- exporter/main.py
+++ exporter/main.py
@@ -2,18 +2,39 @@ import logging
 from datetime import datetime
 from exporter import config
 from exporter.play_store import script as play_store_script
+from exporter.app_store import script as app_store_script
+from exporter.apps_flyer import script as apps_flyier_script
+from exporter.sensortower import script as sensortower_script
+
 
 logger = logging.getLogger(__name__)
 
 
 def run():
     get_play_store_export()
+    get_app_store_export()
+    get_apps_flyier_export()
+    get_sensortower_export()
 
 
 def get_play_store_export():
-    logger.info("Reading report from last run call")
+    logger.info("Play Store task")
     play_store_script.run(config.DEFAULT_EXPORT_FROM, datetime.now())
-    logger.info("Task success")
+
+
+def get_app_store_export():
+    logger.info("App Store task")
+    app_store_script.run(config.DEFAULT_EXPORT_FROM, datetime.now())
+
+
+def get_apps_flyier_export():
+    logger.info("Apps Flyier task")
+    apps_flyier_script.run(config.DEFAULT_EXPORT_FROM, datetime.now())
+
+
+def get_sensortower_export():
+    logger.info("Sensor Tower task")
+    sensortower_script.run(config.DEFAULT_EXPORT_FROM, datetime.now())
 
 
 if __name__ == "__main__":
--- tests/test_main.py
+++ tests/test_main.py
@@ -3,6 +3,14 @@ from exporter import main
 
 
 @mock.patch("exporter.main.get_play_store_export")
-def test_run_do_not_fail(play_store_mock):
+@mock.patch("exporter.main.get_app_store_export")
+@mock.patch("exporter.main.get_apps_flyier_export")
+@mock.patch("exporter.main.get_sensortower_export")
+def test_run_do_not_fail(
+    play_store_mock, app_store_mock, apps_flyier_mock, sensortower_mock
+):
     main.run()
     assert play_store_mock.called
+    assert app_store_mock.called
+    assert apps_flyier_mock.called
+    assert sensortower_mock.called
