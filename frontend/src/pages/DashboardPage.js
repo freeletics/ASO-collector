8e89940934c715ef7a0a0a25dd9ce6eb9aea1b95
Marcin Kopec <marcinkopec@GLOVO-C294.local>
2019-11-03 14:15:41 +0100

    [ASO-9]: Update main page.

--- frontend/src/pages/DashboardPage.js
+++ frontend/src/pages/DashboardPage.js
@@ -1,12 +1,22 @@
 import Page from '../components/Page';
 import React from 'react';
 import OverviewFilters from '../components/Filters/OverviewFilters';
+import Installs from '../components/Boxes/Installs';
+import Reviews from '../components/Boxes/Reviews';
+import Rating from '../components/Boxes/Ratings';
+import RatingComparison from '../components/Boxes/RatingComparison';
+import FeaturingList from '../components/Boxes/FeaturingList'
 
 class DashboardPage extends React.Component {
   render() {
     return (
       <Page className="DashboardPage" title="Regional">
         <OverviewFilters>
+          <Installs></Installs>
+          <FeaturingList></FeaturingList>
+          <RatingComparison></RatingComparison>
+          <Rating></Rating>
+          <Reviews></Reviews>
         </OverviewFilters>
       </Page>
     );
