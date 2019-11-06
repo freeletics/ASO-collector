99348282315eafd229a8e7507c395875dd0e996a
Marcin Kopec <kopecms@gmail.com>
2019-11-06 21:37:32 +0100

    [ASO-8]: Read last saved date from export.

--- exporter/utils/func.py
+++ exporter/utils/func.py
@@ -1,9 +1,20 @@
 import os
+import csv
+import moment
 
 
 def touch(filename):
     with open(filename, "a"):
         os.utime(filename, None)
+
+
+def get_last_date(export_from, filename):
+    try:
+        with open(filename, mode="r") as file:
+            reader = csv.DictReader(file)
+            return max([moment.date(row["date"]).date for row in reader])
+    except (ValueError, FileNotFoundError):
+        return export_from
 
 
 def convertion_rate(downloads, denominator):
