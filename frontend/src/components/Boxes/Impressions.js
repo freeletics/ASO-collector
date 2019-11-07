59fb98c445bb21df3422bcd546059d2627b0bf27
Marcin Kopec <kopecms@gmail.com>
2019-11-07 23:39:59 +0100

    [ASO-9]: Add impressions to detail page.

---
+++ frontend/src/components/Boxes/Impressions.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import { Card, CardBody, Col, Row } from 'reactstrap';
+import { Bar } from 'react-chartjs-2';
+import BoxHeader from '../Widget/BoxHeader';
+import CountryChart from '../Widget/CountryChart';
+import withFilters from '../Filters/withFilters';
+import { defaultOptions, getChartData } from '../../services/chartService';
+import { dateLabels, getSums, getCountries } from './utils';
+
+class Impressions extends React.Component {
+  render() {
+    const { dataIos, colSizes } = this.props;
+    return (
+      <div>
+        <Row>
+          <Col {...colSizes} md={12}>
+            <Card>
+              <CardBody>
+                <BoxHeader>Organic impressions by country (IOS)</BoxHeader>
+                <CountryChart
+                  component={Bar}
+                  data={{
+                    labels: dateLabels(dataIos()),
+                    datasets: getCountries(this.props, dataIos(), 'organic'),
+                  }}
+                  options={defaultOptions}
+                />
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col {...colSizes} md={12}>
+            <Card>
+              <CardBody>
+                <BoxHeader>
+                  Organic impressions Browsers by country (iOS)
+                </BoxHeader>
+                <CountryChart
+                  component={Bar}
+                  data={{
+                    labels: dateLabels(dataIos()),
+                    datasets: getCountries(this.props, dataIos(), 'browsers'),
+                  }}
+                  options={defaultOptions}
+                />
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col {...colSizes} md={12}>
+            <Card>
+              <CardBody>
+                <BoxHeader>
+                  Organic impressions Searchers by country (iOS)
+                </BoxHeader>
+                <CountryChart
+                  component={Bar}
+                  data={{
+                    labels: dateLabels(dataIos()),
+                    datasets: getCountries(
+                      this.props,
+                      dataIos(),
+                      'organic_searchers',
+                    ),
+                  }}
+                  options={defaultOptions}
+                />
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col {...colSizes} md={12}>
+            <Card>
+              <CardBody>
+                <BoxHeader>Impressions by type (IOS)</BoxHeader>
+                <Bar
+                  options={defaultOptions}
+                  data={getChartData(dateLabels(dataIos()), {
+                    Browsers: getSums(this.props, dataIos(), 'browsers'),
+                    Searchers: getSums(
+                      this.props,
+                      dataIos(),
+                      'organic_searchers',
+                    ),
+                    'Other Paid': getSums(this.props, dataIos(), 'other_paid'),
+                    'Search Ads': getSums(this.props, dataIos(), 'search_ads'),
+                  })}
+                />
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default withFilters(Impressions, ['app_store_impressions', 'dataIos']);
--- frontend/src/pages/DetailPage.js
+++ frontend/src/pages/DetailPage.js
@@ -2,12 +2,14 @@ import Page from '../components/Page';
 import React from 'react';
 import OverviewFilters from '../components/Filters/OverviewFilters';
 import Downloads from '../components/Boxes/Downloads'
+import Impressions from '../components/Boxes/Impressions'
 
 class DashboardPage extends React.Component {
   render() {
     return (
       <Page className="DashboardPage" title="Detail">
         <OverviewFilters>
+          <Impressions></Impressions>
           <Downloads></Downloads>
         </OverviewFilters>
       </Page>
