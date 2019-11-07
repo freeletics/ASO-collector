f931135c57f8d2413643f0825111e8d56125c3eb
Marcin Kopec <kopecms@gmail.com>
2019-11-07 23:48:34 +0100

    [ASO-9]: Add page views to detail page.

--- frontend/src/pages/DetailPage.js
+++ frontend/src/pages/DetailPage.js
@@ -3,12 +3,14 @@ import React from 'react';
 import OverviewFilters from '../components/Filters/OverviewFilters';
 import Downloads from '../components/Boxes/Downloads'
 import Impressions from '../components/Boxes/Impressions'
+import PageViews from '../components/Boxes/PageViews'
 
 class DashboardPage extends React.Component {
   render() {
     return (
       <Page className="DashboardPage" title="Detail">
         <OverviewFilters>
+          <PageViews></PageViews>
           <Impressions></Impressions>
           <Downloads></Downloads>
         </OverviewFilters>
