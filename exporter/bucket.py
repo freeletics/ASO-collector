1bbd40dcee9aa3b2a9d8882c1d21c31e6a6ec8ba
Marcin Kopec <kopecms@gmail.com>
2019-11-12 21:57:32 +0100

    [ASO-9]: Allow specify download directory.

--- exporter/bucket.py
+++ exporter/bucket.py
@@ -35,8 +35,9 @@ class BucketAws(Bucket):
     def get_all_objects(self):
         return self.bucket.objects.all()
 
-    def download_file(self, file_name):
-        full_dir = self.get_output_dir(config.RAW_DATA_DIR)
+    def download_file(self, file_name, download_to=None):
+        download_to = download_to or config.RAW_DATA_DIR
+        full_dir = self.get_output_dir(download_to)
         self.resource.Object(self.name, file_name).download_file(
             os.path.join(full_dir, file_name)
         )
@@ -63,7 +64,8 @@ class BucketGcp(Bucket):
     def get_all_objects(self):
         return self.bucket.list_blobs()
 
-    def download_file(self, file_name):
+    def download_file(self, file_name, download_to=None):
+        download_to = download_to or config.RAW_DATA_DIR
         full_dir = self.get_output_dir(config.RAW_DATA_DIR)
         bucket = self.storage_client.get_bucket(self.name)
         blob = bucket.blob(file_name)
